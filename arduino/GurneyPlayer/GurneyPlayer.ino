174f59ead2208e821d62f05e21d576b7344c510e
Mike Watson <mwatson@scriptingdragon.com>
2014-04-24 00:55:50 -0700

    Added non-blockng button and serial logic. Improved the boot sequence.

--- arduino/GurneyPlayer/GurneyPlayer.ino
+++ arduino/GurneyPlayer/GurneyPlayer.ino
@@ -15,12 +15,22 @@ file is played.
 
 //VARIABLES 
 unsigned char commandBuffer[10];
+unsigned char returnBuffer[10];
 const int buttonPin = 2;
 const int ledPin = 3;
 int buttonState;
 int lastButtonState = LOW;
 long lastDebounceTime = 0;
 long debounceDelay = 50;
+long lastFrequencyAdjust = 0;
+long frequencyDelay = 10;
+long lastPlayStatusTime = 0;
+long playStatusDelay = 500;
+int ledFrequency = 0;
+boolean playerConnected = false;
+boolean brightnessUp = true;
+boolean readOnce = false;
+boolean pulseMode = false;
 
 //software serial setup
 SoftwareSerial mySerial(7,8);
@@ -34,8 +44,28 @@ void sendCmd(unsigned char *inputCommands, unsigned len) {
   }
 }
 
+void pulseLed() {
+//gently pulse the led in the button
+  if(millis()-lastFrequencyAdjust>frequencyDelay){
+    lastFrequencyAdjust=millis();
+    if(ledFrequency<255 && brightnessUp) {
+      ledFrequency+=5;
+      if(ledFrequency==255) {
+       brightnessUp=false; 
+      }
+    }
+    else {
+     ledFrequency-=5;
+     if(ledFrequency==0) {
+      brightnessUp=true; 
+     }
+    }
+    analogWrite(ledPin, ledFrequency);
+  }
+}
+
 void setLed(boolean state) {
-//accepts a boolean value for turning on and off the status LED
+//accepts a boolean value for turning on and off the LED in the button
  if(state == 0) {
   digitalWrite(ledPin, LOW);
  } 
@@ -91,22 +121,57 @@ void playNext() {
   commandBuffer[3] = 0x7E;
   sendCmd(commandBuffer, 4);
 }
+
+unsigned char getPlayState() {
+  unsigned char readValue;
+  boolean returnValue = -1;
+  if(readOnce == false) {
+    //readOnce = true;
+    commandBuffer[0] = 0x7E;
+    commandBuffer[1] = 0x02;
+    commandBuffer[2] = 0xC2;
+    commandBuffer[3] = 0x7E;
+    sendCmd(commandBuffer, 4);
+    
+    if(mySerial.available()){
+      while(mySerial.available()>0){
+        readValue = mySerial.read();
+        //Serial.write(mySerial.available());
+        Serial.print(readValue);
+        if(readValue == 2) {
+          Serial.write("\nstopped");
+          returnValue = 0;
+        }
+        else if(readValue == 1) {
+         Serial.write("\nplaying"); 
+         returnValue = 1;
+        }
+        Serial.write("\n");
+      }
+      Serial.write("done\n");
+     }
+    }
+    return returnValue;
+}
+
 //END FUNCTIONS
 
 void setup(){ 
-delay(1000);
-  mySerial.begin(9600);
   delay(1000);
-  setPlayMode();
-  delay(200);
-  startPlaying();
-  delay(200);
-  stopPlaying();
-  //startPlaying();
+  mySerial.begin(9600);
+  Serial.begin(9600);
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
 }
 void loop(){
+  if(playerConnected == false && mySerial.isListening()) {
+   playerConnected = true;
+   setPlayMode();
+   delay(1000);
+   startPlaying();
+   delay(1000);
+   stopPlaying();
+  }
   int reading = digitalRead(buttonPin);
   
   if(buttonState != lastButtonState) {
@@ -120,12 +185,33 @@ void loop(){
     }
     if (lastButtonState == HIGH && buttonState == LOW){
       playNext();
-      setLed(true);
+      //setLed(true);
     }
     else {
-     setLed(false); 
+     //setLed(false); 
     }
   }
+  
+  if(millis()-lastPlayStatusTime>playStatusDelay) {
+    lastPlayStatusTime = millis();
+    int playState = getPlayState();
+    if(playState == 1) {
+      pulseMode = true;
+    }
+    else if(playState == 0) {
+      pulseMode = false;
+    }
+    Serial.write("pulseMode = "+pulseMode);
+  }
+  
+  if(pulseMode == true) {
+   setLed(true);
+  }
+  else {
+    
+    pulseLed(); 
+  }
+  
 }
 
 
